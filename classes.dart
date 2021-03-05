7ec2d06419b9b2a18203e7e8e428db99e43c21d1
Lennox Luis <71387094+LennoxLuis@users.noreply.github.com>
2021-03-05 18:06:32 +0800

    Update and rename README.md to classes.dart

---
+++ classes.dart
@@ -0,0 +1,14 @@
+void main() {
+ 
+var id = '123abc';
+    var name = 'junjun';
+    var email = 'junjun@wvsu.edu.ph';
+    var password = 'qwerty123456';
+    var gender = 'male';
+    
+
+print ('$name is a student of West Visayas State University.');
+  print ('junjun = $gender .   can be emailed through $email.');
+  print ('Current password access for this account $id is $password.');
+
+}
